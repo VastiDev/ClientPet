f5e068232b9ad183eca210a471947897824ec624
vastidev <Vastiane1@gmail.com>
2023-04-07 19:28:19 -0300

    implementa get por Id.postman ok!

--- src/main/java/br/com/petz/clientepet/cliente/application/api/ClienteDetalhadoResponse.java
+++ src/main/java/br/com/petz/clientepet/cliente/application/api/ClienteDetalhadoResponse.java
@@ -1,22 +1,31 @@
 package br.com.petz.clientepet.cliente.application.api;
 
-import java.time.LocalDate;
-import java.util.UUID;
-
+import br.com.petz.clientepet.cliente.domain.Cliente;
 import br.com.petz.clientepet.cliente.domain.Sexo;
 import lombok.Value;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
 @Value
 public class ClienteDetalhadoResponse {
+    private UUID idCliente;
+    private String nomeCompleto;
+    private String email;
+    private String celular;
+    private String cpf;
+    private Sexo sexo;
+    private LocalDateTime dataHoraDoCadastro;
+    private Boolean aceitaTermos;
 
-	private UUID idCliente;
-	private String nomeCompleto;
-	private String cpf;
-	private String email;
-	private String celular;
-	private String telefone;
-	private Sexo sexo;
-	private Boolean aceitaTermos;
-	private LocalDate dataHoraDoCadastro;
-
+    public ClienteDetalhadoResponse(Cliente cliente) {
+        this.idCliente = cliente.getIdCliente();
+        this.nomeCompleto = cliente.getNomeCompleto();
+        this.email = cliente.getEmail();
+        this.celular = cliente.getCelular();
+        this.cpf = cliente.getCpf();
+        this.sexo = cliente.getSexo();
+        this.aceitaTermos = cliente.getAceitaTermos();
+        this.dataHoraDoCadastro = cliente.getDataHoraDoCadastro();
+    }
 }
--- src/main/java/br/com/petz/clientepet/cliente/infra/ClienteInfraRepository.java
+++ src/main/java/br/com/petz/clientepet/cliente/infra/ClienteInfraRepository.java
@@ -1,6 +1,7 @@
 package br.com.petz.clientepet.cliente.infra;
 
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.stereotype.Repository;
 
@@ -30,4 +31,15 @@ public class ClienteInfraRepository implements ClienteRepository {
 		return todosClientes;
 	}
 
+	
+
+	@Override
+	public Cliente buscaClienteAtravesId(UUID idCliente) {
+		log.info("[inicia] ClienteInfraRepository - buscaClienteAtravesId");
+		Cliente cliente = clienteSpringDataJPARepository.findById(idCliente)
+				.orElseThrow(() -> new RuntimeException("Cliente não encontrado!"));
+		log.info("[finaliza] ClienteInfraRepository - buscaClienteAtravesId");
+		return cliente;
+
+}
 }
