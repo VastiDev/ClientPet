96624219613e064761ac49047edd54384943983a
vastidev <Vastiane1@gmail.com>
2023-04-09 17:44:13 -0300

    implementa repository e infra. final aula 7. postman falhando

--- src/main/java/br/com/petz/clientepet/pet/domain/Pet.java
+++ src/main/java/br/com/petz/clientepet/pet/domain/Pet.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+import br.com.petz.clientepet.pet.application.api.PetRequest;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -11,20 +12,25 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
+
 @Data
 @Entity
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class Pet {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	@Column(columnDefinition = "uuid", name = "idPet", updatable = false, unique = true, nullable = false)
 	private UUID idPet;
+	@NotNull
+	@Column(columnDefinition = "uuid", name = "idClienteTutor", nullable = false)
+	private UUID idClienteTutor;
 	@NotBlank
 	private String nomePet;
 	@Enumerated(EnumType.STRING)
@@ -40,10 +46,27 @@ public class Pet {
 	@NotNull
 	private LocalDate dataNascimento;
 	private Integer peso;
-	
-	
 		
 	private LocalDateTime dataHoraDoCadastro;
 	private LocalDateTime dataHoraDaUltimaAlteracao;
 	
-}	
+	public Pet(UUID idCliente, @Valid PetRequest petRequest) {
+		this.idClienteTutor = idCliente;
+		this.nomePet = petRequest.getNomePet();
+		this.porte = petRequest.getPorte();
+		this.tipo = petRequest.getTipo();
+		this.microchip = petRequest.getMicrochip();
+		this.raça = petRequest.getRaça();
+		this.sexo = petRequest.getSexo();
+		this.pelagemCor = petRequest.getPelagemCor();
+		this.rga = petRequest.getRga();
+		this.dataNascimento = petRequest.getDataNascimento();
+		this.peso = petRequest.getPeso();
+		this.dataHoraDoCadastro = LocalDateTime.now();
+	}
+
+	
+		
+}
+	
+	
