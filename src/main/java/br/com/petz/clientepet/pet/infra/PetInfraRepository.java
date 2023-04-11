24f005eb183b492661cff5a924f127b17a3cbce1
vastidev <Vastiane1@gmail.com>
2023-04-10 21:08:14 -0300

    implementa repository e infra de busca pets por id

--- src/main/java/br/com/petz/clientepet/pet/infra/PetInfraRepository.java
+++ src/main/java/br/com/petz/clientepet/pet/infra/PetInfraRepository.java
@@ -27,9 +27,9 @@ public class PetInfraRepository implements PetRepository {
 	@Override
 	public List<Pet> buscaPetsDoClienteComId(UUID idCliente) {
 		log.info("[start] , PetInfraRepository - buscaPetsDoClienteComId");
+		var pets = petSpringDataJPARepository.findByIdClienteTutor(idCliente);
 		log.info("[finish] , PetInfraRepository - buscaPetsDoClienteComId");
-		
-		return null;
+		return pets;
 	}
 
 }
