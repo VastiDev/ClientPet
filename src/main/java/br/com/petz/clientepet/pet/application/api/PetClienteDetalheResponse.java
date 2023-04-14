f4cee5f453ce7f57ca8040dc3c4f74d59ce4cbef
vastidev <Vastiane1@gmail.com>
2023-04-14 16:45:13 -0300

    implementa busca petPeloId

--- src/main/java/br/com/petz/clientepet/pet/application/api/PetClienteDetalheResponse.java
+++ src/main/java/br/com/petz/clientepet/pet/application/api/PetClienteDetalheResponse.java
@@ -3,12 +3,14 @@ package br.com.petz.clientepet.pet.application.api;
 import java.time.LocalDate;
 import java.util.UUID;
 
+import br.com.petz.clientepet.pet.domain.Pet;
 import br.com.petz.clientepet.pet.domain.Porte;
 import br.com.petz.clientepet.pet.domain.SexoPet;
 import br.com.petz.clientepet.pet.domain.TipoPet;
 import lombok.Value;
 @Value
 public class PetClienteDetalheResponse {
+	
 	private UUID idPet;
 	private String nomePet;
 	private Porte porte;
@@ -20,6 +22,19 @@ public class PetClienteDetalheResponse {
 	private String rga;
 	private LocalDate dataNascimento;
 	private Integer peso;
-		
 
+	public PetClienteDetalheResponse(Pet pet) {
+		this.idPet = pet.getIdPet();
+		this.nomePet = pet.getNomePet();
+		this.porte = pet.getPorte();
+		this.tipo = pet.getTipo();
+		this.microchip = pet.getMicrochip();
+		this.raça = pet.getRaça();
+		this.sexo = pet.getSexo();
+		this.pelagemCor = pet.getPelagemCor();
+		this.rga = pet.getRga();
+		this.dataNascimento = pet.getDataNascimento();
+		this.peso = pet.getPeso();
+		
+	}
 }
